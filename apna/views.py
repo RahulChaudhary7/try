2f0ab8b4e3222ecc31c55789b7e60f264b55f574
RahulChaudhary7 <rs8103559@gmail.com>
2023-12-14 11:38:04 +0530

    add templates files

--- apna/views.py
+++ apna/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def stud(request):
-    return HttpResponse("hello Git")
+    return render(request , "first.html")
